227f4e5233fd123daf8e2a3336eca29d6bf91641
natahiko <n.shkarow@gmail.com>
2021-02-11 14:13:41 +0200

    blog layout

---
+++ js/blog.js
@@ -0,0 +1,6 @@
+const data = [{
+    "img_url": "https://www.verywellfit.com/thmb/LeBe7RNtzbJwyKRmH8ditmJ1NKg=/1500x1020/filters:no_upscale():max_bytes(150000):strip_icc()/Snapwire-Running-27-66babd0b2be44d9595f99d03fd5827fd.jpg",
+    "name": "Заголовок 1",
+    "date": "Пт, 3 лютого",
+    "description": "Lorem ipsum dolor sit hhndkd amet, consectetur bnhjjjk adipiscing"
+}]
